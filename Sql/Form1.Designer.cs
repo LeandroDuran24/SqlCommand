7bb3fef70aef8d124189cd24d4bce6b37c44df5c
Leandro <Leandro@LEANDRODURAN>
2017-01-21 09:49:03 -0400

    sqlCommand

--- Sql/Form1.Designer.cs
+++ Sql/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Insert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NametextBox1 = new System.Windows.Forms.TextBox();
+            this.CiudadtextBox2 = new System.Windows.Forms.TextBox();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(67, 143);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 2;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ciudad";
+            // 
+            // NametextBox1
+            // 
+            this.NametextBox1.Location = new System.Drawing.Point(133, 49);
+            this.NametextBox1.Name = "NametextBox1";
+            this.NametextBox1.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox1.TabIndex = 0;
+            // 
+            // CiudadtextBox2
+            // 
+            this.CiudadtextBox2.Location = new System.Drawing.Point(133, 77);
+            this.CiudadtextBox2.Name = "CiudadtextBox2";
+            this.CiudadtextBox2.Size = new System.Drawing.Size(100, 20);
+            this.CiudadtextBox2.TabIndex = 1;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(158, 143);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.CiudadtextBox2);
+            this.Controls.Add(this.NametextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Insert);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NametextBox1;
+        private System.Windows.Forms.TextBox CiudadtextBox2;
+        private System.Windows.Forms.Button Delete;
     }
 }
 
--- Sql/Form1.cs
+++ Sql/Form1.cs
@@ -6,14 +6,63 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Sql
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con = new SqlConnection(@"Data Source=LeandroDuran;Integrated Security=True");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into table1 values('"+NametextBox1.Text+"','"+CiudadtextBox2.Text+"')";
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Registro insertado correctamente");
+            NametextBox1.Text = "";
+            CiudadtextBox2.Text = "";
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "delete from table1 where Nombre ='" + NametextBox1.Text + "','" + CiudadtextBox2.Text + "')";
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Registro eliminado correctamente");
+                NametextBox1.Text = "";
+                CiudadtextBox2.Text = "";
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ocurrio un error...");
+            }
         }
     }
 }
